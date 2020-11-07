394190213ef038d7167d312cc8e29f6ac14839b1
katerina kulchitskay <rfvb.foto@mail.ru>
2020-11-07 10:43:11 +0200

    first commit

---
+++ src/main/kotlin/Comment.kt
@@ -0,0 +1,44 @@
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+data class Comment(
+    val id: Int = 1,
+    val ownerId: Int = 1,
+    var text: String,
+    var isDeleted: Boolean = false,
+    val date: Long = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC),
+    val comments: MutableList<Comment> = mutableListOf(),
+) : Notable<Comment> {
+
+    override fun addTo(dest: MutableList<Comment>) {
+        if (dest.isEmpty()) dest.add(this.copy(id = 1))
+        else dest.add(this.copy(id = dest.last().id + 1))
+    }
+
+    fun edit(text: String) {
+        this.text = text
+    }
+
+    override fun getCommentById(id: Int): Comment {
+        for (comment in comments) {
+            if (comment.id == id && !comment.isDeleted) return comment
+        }
+        throw CommentIdNotFoundException("Comment Id not found!")
+    }
+
+    override fun getCommentByIdForced(id: Int): Comment {
+        for (comment in comments) {
+            if (comment.id == id) return comment
+        }
+        throw CommentIdNotFoundException("Comment Id not found!")
+    }
+
+    override fun delete() {
+        isDeleted = true
+    }
+
+    override fun restore() {
+        isDeleted = false
+    }
+
+}
---
+++ src/main/kotlin/Notable.kt
@@ -0,0 +1,7 @@
+interface Notable<D> {
+    fun addTo(dest: MutableList<D>)
+    fun delete()
+    fun restore()
+    fun getCommentById(id: Int): Comment
+    fun getCommentByIdForced(id: Int): Comment
+}
